cc4dd05b822b13c3da1ffcc3b97b257920a3f0af
Mark Chipp <mark.chipp@live.ca>
2016-11-14 22:43:25 -0500

    initial commit

---
+++ config/globalVars.js
@@ -0,0 +1,16 @@
+// Created by Mark Chipp 200180985 <mark.chipp@live.ca, mark.chipp@mygeorgian.ca>
+// Created on 14-Nov-2016
+
+
+//global variables for the application
+module.exports = {
+  db: 'mongodb://uname:pass@ds044989.mlab.com:44989/comp2068-thu',
+  secret: 's8df7gyh6jr544j5h6g7f8d90gtbhrnjet6y5u4i3',
+  ids: {
+    facebook: {
+      clientID: '1613638918932493',
+      clientSecret: 'fc6c29e18eb133e53064ed1554cb3e5e',
+      callbackURL: 'http://localhost:3000/facebook/callback'
+    }
+  }
+};
